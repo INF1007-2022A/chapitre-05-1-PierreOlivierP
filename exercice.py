06790bcf142333d44a7358596627a716882a0400
PierreOlivierP <pierre-olivier.piquant@polymtl.ca>
2022-10-18 20:31:16 -0400

    a

--- exercice.py
+++ exercice.py
@@ -8,33 +8,83 @@ from typing import List
 def convert_to_absolute(number: float) -> float:
     if number <0:
         number = number * -1
-    else:
-        pass
     return number
 
 
 def use_prefixes() -> List[str]:
     List = []
     prefixes, suffixe = 'JKLMNOPQ', 'ack'
-    for i in range of prefixes:
+    for i in prefixes:
         List.append(i+suffixe)
     return List
 
 
 def prime_integer_summation() -> int:
-    return 0
+    a = 0
+    i = 3
+    n = 2
+    nombres = [2]
+    resultat = 0
+    while a <99:
+        if n == i:
+            i+=1
+            n = 1
+            a+=1
+            nombres.append(i)
+        if i%n ==0:
+            i+=1
+            n = 1
+        n+=1
+    for i in nombres:
+        resultat += i
+
+
+
+
+    return resultat
 
 
 def factorial(number: int) -> int:
-    return 0
+    factoriel = 1
+    while number >1:
+        factoriel = factoriel*number
+        number-=1
+    return factoriel
 
 
 def use_continue() -> None:
-    pass
 
+    pass
+    """""
+    i = 1
+    a = []
+    while i<10:
+        if i ==5:
+            continue
+            a.append(i)
+        i+=1
+    return a
+    """""
 
 def verify_ages(groups: List[List[int]]) -> List[bool]:
-    return []
+    pass
+    #afaire
+    """bon = []
+    for groupe in groups:
+        if len(groupe) > 3 or len(groupe) < 10:
+            if groupe == 25:
+                bon.append(groups)
+            continue
+        if (max(groupe) < 70 ) and (min(groupe) > 18):
+
+            bon.append(groupe)
+            continue
+        if groupe == 25 and groupe not in bon:
+            bon.append(groupe)
+            continue
+
+
+    return bon"""
 
 
 def main() -> None:
